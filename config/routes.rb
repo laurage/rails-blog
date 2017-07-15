085c3da8401cb261a9a342b38739d422be95d2cc
me <me@mes-MacBook-Pro.home>
2017-07-15 18:59:33 +0100

    user sees post when clicking on it

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+
+  get '/posts/:id', to: 'posts#show'
+  resources :posts, only: [:show]
+
   get 'about', to: 'pages#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
